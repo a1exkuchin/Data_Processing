2c6bcf50862c5b76aee1754f2399b87e1f82b778
a1exkuchin <alexkuchin@list.ru>
2021-03-27 15:22:01 +0500

    Initial commit

--- fotoparser/items.py
+++ fotoparser/items.py
@@ -4,14 +4,23 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-from scrapy.loader.processors import MapCompose, TakeFirst
+from scrapy.loader.processors import Compose, TakeFirst
 
 
-def get_url(url):
-    return url
-
+def parse_params(params):
+    string = '*'.join([i.replace('\n', '').strip().replace('  ', '') for i in params])
+    array = string.strip('*').split('***')
+    result = dict()
+    for elem in array:
+        key, value = elem.split('**')
+        result[key] = value
+    print(result)
+    return result
 
 class FotoparserItem(scrapy.Item):
     name = scrapy.Field(output_processor=TakeFirst())
     photos = scrapy.Field()
+    url = scrapy.Field(output_processor=TakeFirst())
     _id = scrapy.Field()
+    params = scrapy.Field(output_processor=Compose(parse_params))
+    price = scrapy.Field(output_processor=TakeFirst())
